09ea8d3c8c0abbd36d193b8b04e2f9f1bb2a1201
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-08-23 14:14:35 +0430

    config project in order to use mongodb

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,13 +4,19 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+// use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+use Illuminate\Auth\Authenticatable as AuthenticatableTrait;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+class User extends Eloquent implements AuthenticatableContract
 {
+    use AuthenticatableTrait;
     use HasApiTokens, HasFactory, Notifiable;
+
+    protected $connection = 'mongodb';
 
     /**
      * The attributes that are mass assignable.
